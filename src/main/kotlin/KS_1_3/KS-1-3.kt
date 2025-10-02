4189a7793ff371428450e5edf755138741abf62f
t1sh3happy <161638138+t1sh3happy@users.noreply.github.com>
2025-10-02 21:44:01 +0300

    kS-1-3

---
+++ src/main/kotlin/KS_1_3/KS-1-3.kt
@@ -0,0 +1,14 @@
+package org.Evgeniy.KS_1_3
+
+fun main() {
+
+    val year: Int = 1961
+    var hour: Int = 9
+    var minute: Int = 7
+    println("Год полета - $year")
+    println("Час посадки - $hour")
+    println("Минута посадки - $minute")
+    hour = 10
+    minute = 55
+    println("Время посадки - $hour:$minute")
+}
