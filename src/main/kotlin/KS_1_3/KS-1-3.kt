c7bdfd2bd7cb4dcaed7e9cccb9ad9585bc7ef927
t1sh3happy <161638138+t1sh3happy@users.noreply.github.com>
2025-10-02 23:30:31 +0300

    Доработан вариант в строку, добавлены нули перед цифрами

--- src/main/kotlin/KS_1_3/KS-1-3.kt
+++ src/main/kotlin/KS_1_3/KS-1-3.kt
@@ -5,9 +5,7 @@ fun main() {
     val year: Int = 1961
     var hour: Int = 9
     var minute: Int = 7
-    println("Год полета - $year")
-    println("Час посадки - $hour")
-    println("Минута посадки - $minute")
+    println("Год полета - $year Час посадки - ${String.format("%02d", hour)} Минута посадки - ${String.format("%02d", minute)}")
     hour = 10
     minute = 55
     println("Время посадки - $hour:$minute")
