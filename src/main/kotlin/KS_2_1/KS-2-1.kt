02c0e4fe4bd9a2f2d1a6272d548887442501e249
t1sh3happy <161638138+t1sh3happy@users.noreply.github.com>
2025-10-12 14:22:28 +0300

    Доработаны замечания

--- src/main/kotlin/KS_2_1/KS-2-1.kt
+++ src/main/kotlin/KS_2_1/KS-2-1.kt
@@ -1,11 +1,11 @@
 package org.Evgeniy.KS_2_1
 
 fun main() {
-    val gradeFirst: Byte = 3
-    val gradeSecond: Byte = 4
+    val gradeFirst = 3
+    val gradeSecond = 4
     val gradeThird: Byte = 3
     val gradeFourth: Byte = 5
-
-    val averageGrade = (gradeFirst + gradeSecond + gradeThird + gradeFourth) / 4.0
+    val totalStudents = 4
+    val averageGrade = (gradeFirst + gradeSecond + gradeThird + gradeFourth).toDouble() / totalStudents
     println(String.format("%.2f", averageGrade))
 }
