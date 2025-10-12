1ddefc841d026e53969840a52cdf915543584343
t1sh3happy <161638138+t1sh3happy@users.noreply.github.com>
2025-10-12 13:53:14 +0300

    Доработаны замечания

--- src/main/kotlin/KS_1_5/KS-1-5.kt
+++ src/main/kotlin/KS_1_5/KS-1-5.kt
@@ -5,17 +5,15 @@ package org.Evgeniy.KS_1_5
 //- Переведи в коде количество секунд в целые минуты и целые часы, сохраняя остаток секунд и минут в новые переменные;
 //- Выведи в консоль время, проведенное в космосе в формате [часы:минуты:секунды], например так: 01:30:09;
 //- Значения часов, минут и секунд отображать двумя цифрами.
-const val SECONDINMINUTE = 60
-const val SECONDINHOUR = 3600
+const val SECOND_IN_MINUTE = 60
+const val SECOND_IN_HOUR = 3600
 
 fun main() {
-
-
     val secondsInSpace: Short = 6480
-    val hours: Short = (secondsInSpace / SECONDINHOUR).toShort()
-    val remainSecondsAfterHour: Short = (secondsInSpace % SECONDINHOUR).toShort()
-    val minutes: Short = (remainSecondsAfterHour / SECONDINMINUTE).toShort()
-    val seconds: Short = (remainSecondsAfterHour % SECONDINMINUTE).toShort()
+    val hours: Short = (secondsInSpace / SECOND_IN_HOUR).toShort()
+    val remainSecondsAfterHour: Short = (secondsInSpace % SECOND_IN_HOUR).toShort()
+    val minutes: Short = (remainSecondsAfterHour / SECOND_IN_MINUTE).toShort()
+    val seconds: Short = (remainSecondsAfterHour % SECOND_IN_MINUTE).toShort()
 
     println(String.format("%02d:%02d:%02d", hours, minutes, seconds))
 }
