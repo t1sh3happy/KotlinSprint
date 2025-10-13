d322ebee704b51d52fdeded73933b9769e137bad
t1sh3happy <161638138+t1sh3happy@users.noreply.github.com>
2025-10-13 22:49:17 +0300

    KS-3-1

---
+++ src/main/kotlin/KS_3_1/KS-3-1.kt
@@ -0,0 +1,12 @@
+package org.Evgeniy.KS_3_1
+
+fun main() {
+
+    val userName = "Евгений"
+
+    var greeting = "Доброе утро, $userName"
+    println(greeting)
+
+    greeting = "Добрый вечер, $userName"
+    println(greeting)
+}
