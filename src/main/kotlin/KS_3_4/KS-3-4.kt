67d220fafb7bc58dfb259d77b93396b72bae053d
t1sh3happy <161638138+t1sh3happy@users.noreply.github.com>
2025-10-18 18:58:40 +0300

    Доработал задание 4 урока 3

--- src/main/kotlin/KS_3_4/KS-3-4.kt
+++ src/main/kotlin/KS_3_4/KS-3-4.kt
@@ -6,15 +6,15 @@ fun main() {
     var from = "$vertical$horizontal"
     var to = "$vertical${4}"
     var moveNumber = 1
-    var move = "$from-$to, номер хода - $moveNumber"
-    println(move)
+    println("[$from-$to;$moveNumber]")
+
 
     vertical = 'D'
     horizontal = 2
     from = "$vertical$horizontal"
     to = "$vertical${horizontal + 1}"
     moveNumber++
-    move = "$from-$to, номер хода - $moveNumber"
-    println("Следующий ход белых $move")
+    print("Следующий ход белых - ")
+    println("[$from-$to;$moveNumber]")
 
 }
