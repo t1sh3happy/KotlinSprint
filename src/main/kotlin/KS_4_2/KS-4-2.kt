ecd05c493039ef099ecc2437b60ef6a5bebe85b0
t1sh3happy <161638138+t1sh3happy@users.noreply.github.com>
2025-10-19 20:36:43 +0300

    Доработал задание 2 урока 4 в соответствии с замечаниями

--- src/main/kotlin/KS_4_2/KS-4-2.kt
+++ src/main/kotlin/KS_4_2/KS-4-2.kt
@@ -1,13 +1,27 @@
 package org.Evgeniy.KS_4_2
 
+const val MINIMUM_WEIGHT_OF_AVERAGE = 35
+const val MAXIMUM_WEIGHT_OF_AVERAGE = 100
+const val SIZE_OF_AVERAGE = 100
 fun main() {
-    val minimumWeightOfAverage = 35
-    val maximumWeightOfAverage = 100
-    val sizeOfAverage = 100
     var inputWeight = 20
     var inputSize = 80
-    println("Груз весом $inputWeight кг и объемом $inputSize л соответствует категории 'Average': ${inputWeight > minimumWeightOfAverage && inputWeight <= maximumWeightOfAverage && inputSize <= sizeOfAverage}")
+    println(
+        "Груз весом $inputWeight кг и объемом $inputSize л соответствует категории 'Average': " +
+            "${
+                inputWeight > MINIMUM_WEIGHT_OF_AVERAGE &&
+                    inputWeight <= MAXIMUM_WEIGHT_OF_AVERAGE &&
+                    inputSize <= SIZE_OF_AVERAGE
+            }"
+    )
     inputWeight = 50
     inputSize = 100
-    println("Груз весом $inputWeight кг и объемом $inputSize л соответствует категории 'Average': ${inputWeight > minimumWeightOfAverage && inputWeight <= maximumWeightOfAverage && inputSize <= sizeOfAverage}")
+    println(
+        "Груз весом $inputWeight кг и объемом $inputSize л соответствует категории 'Average': " +
+            "${
+                inputWeight > MINIMUM_WEIGHT_OF_AVERAGE &&
+                    inputWeight <= MAXIMUM_WEIGHT_OF_AVERAGE &&
+                    inputSize <= SIZE_OF_AVERAGE
+            }"
+    )
 }
