db28756e5b191ac8864385da0d257f7d27287de5
t1sh3happy <161638138+t1sh3happy@users.noreply.github.com>
2025-10-19 22:29:32 +0300

    Подготовил решение 5 задачи урока 4

--- src/main/kotlin/KS_4_5/KS-4-5.kt
+++ src/main/kotlin/KS_4_5/KS-4-5.kt
@@ -2,17 +2,17 @@ package org.Evgeniy.KS_4_5
 
 fun main() {
     print("Имеются повреждения корабля? (true/false): ")
-    val isDamage = readLine()?.toBoolean()
+    val isDamage = readLine()?.toBoolean() ?: false
     print("Введите количество экипажа: ")
     val crewCount = readLine()?.toIntOrNull() ?: 0
     print("Введите количество ящиков с провизией на борту: ")
     val provisionBoxes = readLine()?.toIntOrNull() ?: 0
     print("Благоприятна погода? (true /false): ")
-    val weatherToday = readLine()?.toBoolean()
+    val weatherToday = readLine()?.toBoolean() ?: false
 
     val canSail =
-        (isDamage == false && crewCount >= 55 && crewCount <= 50 && provisionBoxes > 50 && weatherToday == true) ||
-            (isDamage == true && crewCount == 70 && weatherToday == true && provisionBoxes >= 50)
+        (!isDamage && crewCount in 55..70 && provisionBoxes > 50) ||
+            (isDamage && crewCount == 70 && weatherToday == true && provisionBoxes >= 50)
     if (canSail) {
         println("Корабль может отправиться в плавание")
     } else {
