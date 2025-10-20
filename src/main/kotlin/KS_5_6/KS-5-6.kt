e7890ae7b23ddad79693ec390160805a7addefde
t1sh3happy <161638138+t1sh3happy@users.noreply.github.com>
2025-10-21 00:03:20 +0300

    Подготовил решение 6 задачи урока 5

---
+++ src/main/kotlin/KS_5_6/KS-5-6.kt
@@ -0,0 +1,21 @@
+package org.Evgeniy.KS_5_6
+
+const val UNDERWEIGHT = 18.5
+const val NORMAL_BODY_WEIGTH = 24.9
+const val OVERWEIGHT = 30.0
+const val CENTIMETERS_IN_METER = 100
+
+fun main() {
+    print("Введите ваш вес в кг : ")
+    val inputWeight = readln().toDouble()
+    print("Введите ваш рост в см : ")
+    val inputHeight = readln().toDouble()
+    val bodyMassIndex = inputWeight / ((inputHeight / CENTIMETERS_IN_METER) * (inputHeight / CENTIMETERS_IN_METER))
+    print("Ваш индекс составляет : %.2f. Категория веса - ".format(bodyMassIndex))
+    when {
+        bodyMassIndex < UNDERWEIGHT -> println("Недостаточная масса тела")
+        bodyMassIndex in UNDERWEIGHT..NORMAL_BODY_WEIGTH -> println("Нормальная масса тела")
+        bodyMassIndex in (NORMAL_BODY_WEIGTH + 0.1)..OVERWEIGHT -> println("Избыточная масса тела")
+        bodyMassIndex > OVERWEIGHT -> println("Ожирение")
+    }
+}
