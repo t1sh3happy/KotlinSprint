bf7a3eebec82c6d17f73d687eaf5688b305ea06d
t1sh3happy <161638138+t1sh3happy@users.noreply.github.com>
2025-10-20 22:18:15 +0300

    Доработал решение 1 задачи урока 5

--- src/main/kotlin/KS_5_1/KS-5-1.kt
+++ src/main/kotlin/KS_5_1/KS-5-1.kt
@@ -1,10 +1,11 @@
 package org.Evgeniy.KS_5_1
 
 fun main() {
-    val a = (Math.random() * 10).toInt()
-    val b = (Math.random() * 10).toInt()
-    print("Введите решение примера $a + $b : ")
-    val c = readLine()!!.toInt()
-    if (a + b == c) println("Добро пожаловать!")
-    else println("Доступ запрещен")
+    val firstTerm = (0..10).random()
+    val secondTerm = (0..10).random()
+    print("Введите решение примера $firstTerm + $secondTerm : ")
+    val total = readln().toInt()
+
+    if (firstTerm + secondTerm == total) println("Добро пожаловать!")
+     else println("Доступ запрещен")
 }
