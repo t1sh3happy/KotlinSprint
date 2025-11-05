b941e53de915cc62d60596708e1a3fb4d9739a67
Evgeniy <t1sh3happy@gmail.com>
2025-11-05 22:43:39 +0300

    Убрал систему логин/пароль, поставил счётчик в правильное место, перенес строку

--- src/main/kotlin/KS_6_5/KS-6-5.kt
+++ src/main/kotlin/KS_6_5/KS-6-5.kt
@@ -2,34 +2,23 @@ package org.Evgeniy.KS_6_1
 
 fun main() {
 
-    print("Создайте логин : ")
-    val login = readln()
-    print("Создайте пароль : ")
-    val password = readln()
-    println("Вы успешно зарегистрировались")
-
-    do {
-        print("Введите логин : ")
-        val inputLog = readln()
-        print("Введите пароль : ")
-        val inputPass = readln()
-
-    } while (inputLog != login || inputPass != password)
     var counter = 3
     do {
         val firstNumber = (1..9).random()
         val secondNumber = (1..9).random()
         val sum = firstNumber + secondNumber
-        print("Введите сумму чисел $firstNumber и $secondNumber ")
+        print("Введите сумму чисел $firstNumber и $secondNumber : ")
         val inputNumber = readln().toInt()
-        counter--
 
         if (inputNumber == sum) {
             println("Добро пожаловать!")
             break
         } else if (counter == 0) {
             println("Доступ запрещен")
-        } else {println("Неверно, предлагаю решить заново. Оставшееся количество попыток $counter")}
+        } else {
+            counter--
+            println("Неверно, предлагаю решить заново. Оставшееся количество попыток $counter")
+            }
 
     } while (counter > 0)
 
