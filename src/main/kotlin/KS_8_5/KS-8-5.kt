2e6351217ebee2f5a39c0f6f06f4b476821a8353
kes <t1sh3happy@gmail.com>
2026-01-19 23:31:21 +0300

    Доработал решение к задаче 8-5

--- src/main/kotlin/KS_8_5/KS-8-5.kt
+++ src/main/kotlin/KS_8_5/KS-8-5.kt
@@ -3,15 +3,15 @@ package org.Evgeniy.KS_8_5
 fun main() {
     print("Введите количество ингридиентов: ")
     val userNumber = readln().toInt()
-    var userArray = Array<String>(userNumber) { "" }
+    val userArray = Array<String>(userNumber) { "" }
 
-    for (ingredient in userArray) {
-        println("Введите ингридиент ${userArray.indexOf(ingredient) + 1}: ")
-        userArray[userArray.indexOf(ingredient)] = readln()
+    for (ingredient in userArray.indices) {
+        println("Введите ингридиент ${ingredient + 1}: ")
+
+        userArray[ingredient] = readln()
 
     }
 
-    for (ingredient in userArray)
-        println("Ингредиент ${userArray.indexOf(ingredient) + 1} : $ingredient")
+    for (ingredient in userArray) println("Ингредиент ${userArray.indexOf(ingredient) + 1} : $ingredient")
 
 }
