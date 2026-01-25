eec3d3d577d3cb644268830ac229d94d60472459
kes <t1sh3happy@gmail.com>
2026-01-25 22:32:08 +0300

    Доработал задачу 9-4

--- src/main/kotlin/KS_9_4/KS-9-4.kt
+++ src/main/kotlin/KS_9_4/KS-9-4.kt
@@ -1,12 +1,9 @@
 package org.Evgeniy.KS_9_4
 
 fun main() {
-    val ingredientsForPlov = arrayOf<String>()
-    print("Введите 5 ингредиентов, перечисленных через “, ” (запятая с пробелом): ")
-    val usersIngredients = readln()
-    val listOfIngredients = usersIngredients.split(", ")
-    val sortedList = listOfIngredients.sorted()
-    sortedList.forEach {
+    print("Введите 5 ингредиентов, перечисленных через \", \" (запятая с пробелом): ")
+    val listOfIngredients = readln().split(", ").sorted()
+    listOfIngredients.forEach {
         println(it)
     }
 
