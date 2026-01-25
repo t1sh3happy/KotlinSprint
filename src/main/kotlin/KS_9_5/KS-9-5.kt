3952e7dfe72c8bfd5b07400247150df130b78ef7
kes <t1sh3happy@gmail.com>
2026-01-25 22:41:11 +0300

    Доработал задачу 9-5

--- src/main/kotlin/KS_9_5/KS-9-5.kt
+++ src/main/kotlin/KS_9_5/KS-9-5.kt
@@ -1,26 +1,20 @@
 package org.Evgeniy.KS_9_5
 
 fun main() {
-    val ingredientsSet =
-        mutableSetOf<String>()
-
+    val ingredientsSet = mutableSetOf<String>()
 
     var countIngredients = 0
-    while(countIngredients < 5) {
+    while (countIngredients < 5) {
         print("Введите ингредиент ${countIngredients + 1} :")
-        val userIngredient = readln()
+        val userIngredient = readln().trim()
         ingredientsSet.add(userIngredient.lowercase())
         countIngredients++
     }
 
     val result = ingredientsSet
         .sorted()
-        .mapIndexed { index, string ->
-            if (index == 0) {
-                string.replaceFirstChar { it.uppercase() }
-                } else { string }
-        }
-        .joinToString (", ")
+        .joinToString(", ")
+        .replaceFirstChar { it.uppercase() }
     println(result)
 
 }
