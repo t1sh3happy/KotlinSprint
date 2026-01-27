a0954e2599bdad65cf6ec0453f4ff596231c2402
kes <t1sh3happy@gmail.com>
2026-01-27 22:46:51 +0300

    Доработал решение задачи 10-4

--- src/main/kotlin/KS_10_2/KS-10-2.kt
+++ src/main/kotlin/KS_10_2/KS-10-2.kt
@@ -1,23 +1,19 @@
 package org.Evgeniy.KS_10_2
 
+const val MINIMUM_LENGTH = 4
+
 fun main() {
     print("Введите логин: ")
     val userLogin = readln()
     print("Введите пароль: ")
     val userPass = readln()
-    if (validation(userLogin) && validation(userPass))
-    {
+    if (validation(userLogin) && validation(userPass)) {
         println("Добро пожаловать")
+    } else {
+        println("Логин или пароль недостаточно длинные")
     }
-    else   {println("Логин или пароль недостаточно длинные")}
 }
 
 fun validation(input: String): Boolean {
-
-    val count = input.length
-    if (count > 4) {
-        return true
-    } else {
-        return false
-    }
+    return input.length > MINIMUM_LENGTH
 }
