a5c01a53d0ffd887f6f7c9348f41b2f4bf60cf30
kes <t1sh3happy@gmail.com>
2026-03-04 16:37:46 +0300

    Доработал решение задачи 11-3

--- src/main/kotlin/KS_11_3/KS-11-3.kt
+++ src/main/kotlin/KS_11_3/KS-11-3.kt
@@ -1,8 +1,6 @@
 package org.Evgeniy.KS_11_3
 
-
 data class User(
-
     val nickname: String,
     val avatarURL: String,
     var status: SpeakerStatus = SpeakerStatus.MIC_OFF,
@@ -14,12 +12,7 @@ enum class SpeakerStatus {
     SILENCED
 }
 
-class Room(
-    val nameFeed: String,
-    val coverFeed: String,
-
-    ) {
-    val participantList: MutableMap<String, User> = mutableMapOf()
+class Room(val name: String, val cover: String, val participantList: MutableMap<String, User> = mutableMapOf()) {
     fun addParticipant(user: User) {
         participantList[user.nickname] = user
     }
@@ -34,8 +27,8 @@ fun main() {
     val user2 = User("KekChelovek", "www.baton.net/somewhereurl/user/KekChelovek", SpeakerStatus.MIC_OFF)
     val user3 = User("Student", "www.baton.net/somewhereurl/user/Student", SpeakerStatus.MIC_OFF)
     val user4 = User("UralVagonMash", "www.baton.net/somewhereurl/user/UralVagonMash", SpeakerStatus.MIC_OFF)
-
     val room1 = Room("Hello_There", "www.baton.net/somewhereurl/group/Hello_There")
+
     room1.addParticipant(user1)
     room1.addParticipant(user2)
     println(room1.participantList.values)
@@ -48,5 +41,4 @@ fun main() {
     println(user1.status)
     room1.changeStatus("JohnBaton", SpeakerStatus.SILENCED)
     println(user1.status)
-
 }
