5d96f25211fda2aa40600adfcea2478fda87113e
Kesha <t1sh3happy@gmail.com>
2026-05-11 20:01:33 +0300

    Подготовил решение задачи 15-5

---
+++ src/main/kotlin/KS_15_5/KS-15-5.kt
@@ -0,0 +1,94 @@
+package org.Evgeniy.KS_15_5
+
+fun main() {
+    val car1 = Car()
+    val car2 = Car()
+    val car3 = Car()
+    val truck = Truck()
+    val truck2 = Truck()
+    car1.loadPassengers(4)
+    car2.loadPassengers(3)
+    car3.loadPassengers(3)
+    car1.moveTo()
+    car2.moveTo()
+    car3.moveTo()
+    car1.unloadPassengers(5)
+    car2.unloadPassengers(4)
+    car3.unloadPassengers(3)
+    truck.loadCargo(5)
+    truck.moveTo()
+    truck2.loadPassengers(5)
+    truck2.moveTo()
+    truck.unloadPassengers(5)
+    truck.loadCargo(2)
+    truck2.loadCargo(34)
+    truck.unloadCargo(2)
+    truck2.unloadCargo(3)
+
+}
+
+class Car : Movable,
+    PassengerCarrier {
+    override val maxPassengers: Int = 3
+    override var currentPassengers: Int = 0
+}
+
+class Truck : Movable, CargoCarrier,
+    PassengerCarrier {
+    override val maxCargo: Int = 2
+    override var currentCargo: Int = 0
+    override val maxPassengers: Int = 1
+    override var currentPassengers: Int = 0
+}
+
+interface Movable {
+    fun moveTo() {
+        println("Двигаюсь по дороге")
+    }
+}
+
+interface PassengerCarrier {
+    val maxPassengers: Int
+    var currentPassengers: Int
+    fun loadPassengers(count: Int) {
+        if (currentPassengers + count <= maxPassengers) {
+            currentPassengers += count
+            println("Загружено $count пассажиров. Всего: $currentPassengers/$maxPassengers")
+        } else {
+            println("Не помещается! Свободно: ${maxPassengers - currentPassengers}")
+        }
+    }
+
+    fun unloadPassengers(count: Int) {
+        if (currentPassengers - count >= 0) {
+            currentPassengers -= count
+            println("Выгружено $count пассажиров. Всего: $currentPassengers/$maxPassengers")
+        } else {
+            println("Неправильно введено количество выгружаемых пассажиров")
+        }
+    }
+
+}
+
+interface CargoCarrier {
+    val maxCargo: Int
+    var currentCargo: Int
+    fun loadCargo(count: Int) {
+        if (currentCargo + count <= maxCargo) {
+            currentCargo += count
+            println("Загружено $count т. Всего: $currentCargo/$maxCargo")
+        } else {
+            println("Не помещается! Свободно: ${maxCargo - currentCargo}")
+        }
+    }
+
+    fun unloadCargo(count: Int) {
+        if (count <= currentCargo) {
+            currentCargo -= count
+            println("Выгружено $count т. Всего: $currentCargo/$maxCargo")
+        } else {
+            println("Неправильно введено количество выгружаемого груза")
+        }
+
+    }
+}
