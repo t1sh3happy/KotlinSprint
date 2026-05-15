d030a44742b149749db8f6fa50c43bce6be539df
Kesha <t1sh3happy@gmail.com>
2026-05-15 12:12:14 +0300

    Подготовил решение задачи 16-1

---
+++ src/main/kotlin/KS_16_1/KS-16-1.kt
@@ -0,0 +1,17 @@
+package org.Evgeniy.KS_16_1
+
+import kotlin.random.Random
+
+fun main() {
+    val cube1 = Cube()
+    println("Число кубика ${cube1.getNumberOfCubes()}")
+
+}
+
+class Cube {
+    private var numberOfCubes: Int = Random.nextInt(1, 6)
+    fun getNumberOfCubes(): Int {
+        return numberOfCubes
+    }
+
+}
