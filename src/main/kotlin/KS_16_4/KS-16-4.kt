f7d82fefe87104dbc2e2d628732dce10db406e1c
Kesha <t1sh3happy@gmail.com>
2026-05-16 01:20:33 +0300

    Подготовил решение задачи KS-16-4

---
+++ src/main/kotlin/KS_16_4/KS-16-4.kt
@@ -0,0 +1,27 @@
+package org.Evgeniy.KS_16_4
+
+fun main() {
+    val order1 = Order(1)
+    println(order1.status)
+    sendToManager(order1, "В обработке")
+    println(order1.status)
+
+}
+
+class Order(val number: Int) {
+    var status: String = "Создан"
+        private set
+
+
+    fun changeStatus(newStatus: String) {
+        status = newStatus
+    }
+
+
+}
+
+fun sendToManager(order: Order, newStatus: String) {
+    println("Заявка менеджеру: по заказу ${order.number}, поменять статус на $newStatus")
+    order.changeStatus(newStatus)
+    println("Статус изменен на $newStatus")
+}
