086020b257bb6cce535f245279294b6b4c26029c
Kesha <t1sh3happy@gmail.com>
2026-05-19 19:23:13 +0300

    Доработал решение  задачи KS-16-3

--- src/main/kotlin/KS_16_3/KS-16-3.kt
+++ src/main/kotlin/KS_16_3/KS-16-3.kt
@@ -2,18 +2,14 @@ package org.Evgeniy.KS_16_3
 
 fun main() {
     val user1 = User("login", "password")
-    if (user1.validate("ы")) {
+    if (user1.validate("password")) {
         println("Вы указали верный пароль")
     } else {
         println("Пароль неверный")
     }
 }
 
+class User(private val login: String, private val password: String) {
 
-class User(val login: String, private val password: String) {
-
-
-    fun validate(input: String): Boolean {
-        return password == input
-    }
+    fun validate(input: String): Boolean = password == input
 }
