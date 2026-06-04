451e616a900b227bb1390852bb04f085876a4722
Kesha <t1sh3happy@gmail.com>
2026-06-04 21:20:19 +0300

    Подготовил решение задачи KS-19-3

---
+++ src/main/kotlin/KS_19_3/KS-19-3.kt
@@ -0,0 +1,22 @@
+package org.Evgeniy.KS_19_3
+
+class SpaceShip {
+    fun blastOff() {
+        TODO("напоминание о том, что здесь нужна информация о дополнительной логике")
+    }
+
+    fun landOn() {
+        TODO()
+    }
+
+    fun shootAsteroid() {
+        TODO("напоминание о том, что здесь нужна информация о дополнительной логике")
+    }
+}
+
+fun main() {
+    val spaceShip = SpaceShip()
+    spaceShip.blastOff()
+    spaceShip.landOn()
+    spaceShip.shootAsteroid()
+}
