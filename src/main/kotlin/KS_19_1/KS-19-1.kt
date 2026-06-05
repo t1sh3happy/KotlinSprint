c9b271dd539705a94a28c93226862e0901c1c40c
Kesha <t1sh3happy@gmail.com>
2026-06-05 11:47:42 +0300

    Подготовил решение задачи KS-19-5

--- src/main/kotlin/KS_19_1/KS-19-1.kt
+++ src/main/kotlin/KS_19_1/KS-19-1.kt
@@ -1,12 +1,12 @@
 package org.Evgeniy.KS_19_1
 
-enum class Fish {
-    GUPPY,
-    ANGELFISH,
-    GOLDFISH,
-    SIAMESE_FIGHTING_FISH,
+enum class Fish(val nameFish: String) {
+    GUPPY("Гуппи"),
+    ANGELFISH("Скалярия"),
+    GOLDFISH("Золотая рыбка"),
+    SIAMESE_FIGHTING_FISH("Петушок"),
 }
 
 fun main() {
-println("Вы можете добавить в свой аквариум следующих рыб: ${Fish.entries.joinToString() }}")
+    println("Вы можете добавить в свой аквариум следующих рыб: ${Fish.entries.joinToString { it.nameFish }}")
 }
