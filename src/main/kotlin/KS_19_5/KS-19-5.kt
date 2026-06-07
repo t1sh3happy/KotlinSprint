6662c1decb22e16e6fac9e605647d1c733983231
Kesha <t1sh3happy@gmail.com>
2026-06-07 23:07:18 +0300

    Подготовил решение задачи KS-19-5

--- src/main/kotlin/KS_19_5/KS-19-5.kt
+++ src/main/kotlin/KS_19_5/KS-19-5.kt
@@ -2,26 +2,36 @@ package org.Evgeniy.KS_19_5
 
 enum class Gender {
     MALE,
-    FEMALE
+    FEMALE,
 }
 
-class Person(val name: String, val gender: Gender)
+class Person(val name: String, val gender: Gender) {
 
+    fun printInfo() = println("Имя $name, пол $gender")
 
+}
 
 fun main() {
-val persons = mutableListOf<Person>()
-    println("Введите своё имя а также пол на другой строке в формате ${Gender.entries.joinToString(", ")}")
+    val persons = mutableListOf<Person>()
+    println("Введите своё имя а также пол. Доступные значения: MALE, FEMALE")
     while (persons.size < 5) {
         println("Имя: ")
         val name = readln()
         println("ПОЛ:")
-        val inputGender: String = readln()
+        var inputGender: String = readln()
+
+        while (inputGender != (Gender.MALE.name) && inputGender != (Gender.FEMALE.name)) {
+            println("Вы ошиблись, повторите ввод доступных значений: MALE, FEMALE")
+            inputGender = readln()
+        }
+
         val gender = Gender.valueOf(inputGender)
         val person = Person(name, gender)
         persons.add(person)
     }
 
-    println(persons.joinToString("\n") { "${it.name} — ${it.gender}" })
+    for (person in persons) {
+        person.printInfo()
+    }
 
 }
