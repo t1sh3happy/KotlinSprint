6ccc64941e7c40fb0e6b03b7db33a0693878fb15
Kesha <t1sh3happy@gmail.com>
2026-06-08 19:00:47 +0300

    Подготовил решение задачи KS-20-2

---
+++ src/main/kotlin/KS_20_2/KS-20-2.kt
@@ -0,0 +1,15 @@
+package org.Evgeniy.KS_20_2
+
+class Player(val name: String, var currentHealth: Int, val maxHealth: Int)
+
+val healingPotion: (Player) -> Unit = { it.currentHealth = it.maxHealth }
+
+
+fun main() {
+    val hero = Player("bob", 20, 100)
+    println("Имя игрока: ${hero.name}")
+    println("Максимальное здоровье: ${hero.maxHealth}")
+    println("Текущее здоровье: ${hero.currentHealth}")
+    healingPotion(hero)
+    println("Текущее здоровье: ${hero.currentHealth}")
+}
