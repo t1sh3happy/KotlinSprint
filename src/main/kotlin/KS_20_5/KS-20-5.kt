a57b6bba4533cd5512c29bd4cde5de468ebefc85
Kesha <t1sh3happy@gmail.com>
2026-06-09 11:55:16 +0300

    Подготовил решение задачи KS-20-5

---
+++ src/main/kotlin/KS_20_5/KS-20-5.kt
@@ -0,0 +1,31 @@
+package org.Evgeniy.KS_20_5
+
+class Robot {
+
+    val phrases: List<String> = listOf(
+        "У меня миллион идей, но все они ведут к неминуемой гибели",
+        "Я с вами не разговариваю, у меня депрессия",
+        "Мозг величиной с планету, а меня просят открыть дверь",
+        "Жизнь. Не говорите мне о жизни",
+        "Мне так грустно, что хоть светодиоды выкалывай",
+    )
+
+    fun say() {
+        val randomPhrase = phrases.random()
+        println(modifier?.invoke(randomPhrase) ?: randomPhrase)
+    }
+
+    private var modifier: ((String) -> String)? = null
+
+    fun setModifier(newModifier: ((String) -> String)) {
+        modifier = newModifier
+    }
+
+}
+
+fun main() {
+    val robot = Robot()
+    robot.say()
+    robot.setModifier { it.reversed() }
+    robot.say()
+}
