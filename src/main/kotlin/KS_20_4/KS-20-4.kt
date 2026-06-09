4948ebac6858a0de5ef33a5297870cdc0f1e29ce
Kesha <t1sh3happy@gmail.com>
2026-06-09 12:12:06 +0300

    Подготовил решение задачи KS-20-5

--- src/main/kotlin/KS_20_4/KS-20-4.kt
+++ src/main/kotlin/KS_20_4/KS-20-4.kt
@@ -3,8 +3,10 @@ package org.Evgeniy.KS_20_4
 
 fun main() {
 
-    val strings: List<String> = listOf("Hello", "World", "Kotlin", "Java")
+    val strings: List<String> = listOf("Hello", "World", "Java", "Kotlin", "Python")
 
-    val result = strings.map { it -> { println("Нажат элемент $it") } }
-    result.forEachIndexed { index, show -> if (index % 2 == 0) show() }
+    val result = strings.map { { println("Нажат элемент $it") } }
+    result.forEachIndexed { index, show -> if (index % 2 == 1) show() }
+
+
 }
